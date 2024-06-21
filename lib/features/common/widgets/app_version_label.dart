75ea6f874463451b90233c0814eec8ac5ff919b6
Darien Romero <darien.romero@mediabyte.com.pe>
2024-06-21 15:00:51 -0500

    update app_version_label

--- lib/features/common/widgets/app_version_label.dart
+++ lib/features/common/widgets/app_version_label.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:package_info/package_info.dart';
-import 'package:todo_y_pomodoro_app/core/constants.dart';
+import 'package:shorebird_code_push/shorebird_code_push.dart';
 import 'package:todo_y_pomodoro_app/core/utils.dart';
 
 class AppVersionLabel extends StatefulWidget {
-  const AppVersionLabel({Key? key}) : super(key: key);
+  const AppVersionLabel({super.key});
 
   @override
   State<AppVersionLabel> createState() => _AppVersionLabelState();
 }
 
 class _AppVersionLabelState extends State<AppVersionLabel> {
-  String version = "";
+  final shorebirdCodePush = ShorebirdCodePush();
+  String versionLabel = "";
+  String versionNumber = "0";
+  String? patchVersion = "-";
   
   @override
   void initState() {
@@ -21,12 +24,18 @@ class _AppVersionLabelState extends State<AppVersionLabel> {
 
   Future<void> loadVersion() async {
     PackageInfo packageInfo = await PackageInfo.fromPlatform();
-    version = packageInfo.version;
+    versionLabel = packageInfo.version;
+    versionNumber = packageInfo.buildNumber;
+    final patchNumber = await shorebirdCodePush.currentPatchNumber();
+    if(patchNumber == null){
+      patchVersion = "";
+    }else{
+      patchVersion = "-patch$patchNumber";
+    }
     if(mounted){
       setState(() {});
     }
   }
-
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -37,7 +46,7 @@ class _AppVersionLabelState extends State<AppVersionLabel> {
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
           Text(
-            "V $version + $patchVersion",
+            "v$versionLabel+$versionNumber$patchVersion",
             style: const TextStyle(
               color: Colors.black54
             )
